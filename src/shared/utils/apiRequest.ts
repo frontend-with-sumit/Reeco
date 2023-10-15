a5288880329e67df364ac997f3169313b6c95676
Sumit <sk990005@outlook.com>
2023-10-15 18:33:47 +0530

    add api service

---
+++ src/shared/utils/apiRequest.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosResponse } from "axios";
+
+const baseURL = "http://localhost:3000";
+
+interface ParamsType {
+	url: string;
+	urlParams?: number | string;
+	method?: string;
+	data?: object;
+}
+const makeApiRequest = async ({ url, urlParams, method, data }: ParamsType) => {
+	try {
+		const response: AxiosResponse = await axios({
+			url: baseURL + url + "/" + urlParams,
+			method,
+			data,
+		});
+
+		return response;
+	} catch (err) {
+		if (axios.isAxiosError(err)) throw new Error(err?.message);
+		else throw new Error("Something went wrong");
+	}
+};
+
+export { makeApiRequest };
